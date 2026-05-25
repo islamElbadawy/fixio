d765ba8c04f5fc196dc713354fa194e58ecbec2a
islamElbadawy <islam.201211@gmail.com>
2026-05-25 07:13:08 +0300

    prepare main config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { appConfig, dbConfig, jwtConfig } from './config/app.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig, dbConfig, jwtConfig],
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/config/app.config.ts
@@ -0,0 +1,23 @@
+import { registerAs } from '@nestjs/config';
+
+export const appConfig = registerAs('app', () => ({
+  nadeEnv: process.env.NODE_ENV || 'development',
+  port: parseInt(process.env.PORT ?? '3000', 10),
+  name: process.env.APP_NAME ?? 'Fixio',
+}));
+
+export const dbConfig = registerAs('database', () => ({
+  host: process.env.DB_HOST ?? 'localhost',
+  port: parseInt(process.env.DB_PORT ?? '5432', 10),
+  username: process.env.DB_USERNAME ?? 'postgres',
+  password: process.env.DB_PASSWORD ?? '',
+  name: process.env.DB_NAME ?? 'fixio_db',
+}));
+
+
+export const jwtConfig = registerAs('jwt', () => ({
+  accessSecret: process.env.JWT_ACCESS_SECRET ?? 'dev_secret',
+  accessExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN ?? '15m',
+  refreshSecret: process.env.JWT_REFRESH_SECRET ?? 'dev_refresh_secret',
+  refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN ?? '7d',
+}));
