ae0119c80bf3daa01ac610de547503d51569f8de
islamElbadawy <islam.201211@gmail.com>
2026-05-26 07:01:25 +0300

    Fix db connection and apply migrations and seeding data

--- mikro-orm.config.ts
+++ mikro-orm.config.ts
@@ -1,17 +1,20 @@
 import { defineConfig } from '@mikro-orm/postgresql';
 import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
+import { Migrator } from '@mikro-orm/migrations';
 
 export default defineConfig({
   host: process.env.DB_HOST ?? 'localhost',
   port: parseInt(process.env.DB_PORT ?? '5432', 10),
-  user: process.env.DB_USERNAME ?? 'postgres',
-  password: process.env.DB_PASSWORD ?? '',
+  user: process.env.DB_USERNAME ?? 'openpg',
+  password: process.env.DB_PASSWORD ?? 'openpgpwd',
   dbName: process.env.DB_NAME ?? 'fixio_db',
 
   entities: ['dist/**/*.entity.js'],
   entitiesTs: ['src/**/*.entity.ts'],
 
   metadataProvider: TsMorphMetadataProvider,
+
+  extensions: [Migrator],
 
   migrations: {
     path: 'src/database/migrations',
